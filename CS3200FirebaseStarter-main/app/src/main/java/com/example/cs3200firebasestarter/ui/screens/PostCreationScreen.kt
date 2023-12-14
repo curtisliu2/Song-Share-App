4ecd420dd2cc9aade8464aec3e927caef60d248e
curtisliu2 <92825247+curtisliu2@users.noreply.github.com>
2023-12-13 20:15:07 -0700

    Example UI

--- CS3200FirebaseStarter-main/app/src/main/java/com/example/cs3200firebasestarter/ui/screens/PostCreationScreen.kt
+++ CS3200FirebaseStarter-main/app/src/main/java/com/example/cs3200firebasestarter/ui/screens/PostCreationScreen.kt
@@ -4,6 +4,8 @@ import android.Manifest
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
@@ -13,6 +15,7 @@ import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import com.example.cs3200firebasestarter.ui.viewmodels.LocationViewModel
@@ -39,6 +42,11 @@ fun PostCreationScreen(navHostController: NavHostController) {
     // Initialize Firestore
     val db = FirebaseFirestore.getInstance()
 
+
+    var searchText by remember { mutableStateOf("") }
+    val allItems = listOf("Apple", "Banana", "Cherry", "Date", "Elderberry", "Fig", "Grape", "Honeydew") // Example data
+    val filteredItems = allItems.filter { it.startsWith(searchText, ignoreCase = true) }.take(5) // Filtering and limiting to 5 items
+
     // State variables for each attribute
     var userName by remember { mutableStateOf("") }
     var caption by remember { mutableStateOf("") }
@@ -48,23 +56,39 @@ fun PostCreationScreen(navHostController: NavHostController) {
         verticalArrangement = Arrangement.SpaceAround
 
     ) {
+        Spacer(modifier = Modifier.height(50.dp))
         Text(
             text = "Create Post",
             modifier = Modifier.fillMaxWidth(),
             style = MaterialTheme.typography.headlineLarge,
             textAlign = TextAlign.Center
         )
-        // Form fields
-        OutlinedTextField(value = userName, onValueChange = { userName = it }, label = { Text("Username") })
-        OutlinedTextField(value = caption, onValueChange = { caption = it }, label = { Text("Caption") })
         location?.let { loc ->
             Text(
-                text = loc,
+                text = "Location: $loc",
                 modifier = Modifier.fillMaxWidth(),
                 textAlign = TextAlign.Center
             )
         }
+        Spacer(modifier = Modifier.height(150.dp))
+        // Form fields
+        OutlinedTextField(value = userName, onValueChange = { userName = it }, label = { Text("Username") })
+        OutlinedTextField(value = caption, onValueChange = { caption = it }, label = { Text("Caption") })
         // TODO Spotify stuff here
+        OutlinedTextField(
+            value = searchText,
+            onValueChange = { searchText = it },
+            label = { Text("Search Song") }
+        )
+        LazyColumn (
+            contentPadding = PaddingValues(vertical = 4.dp),
+            verticalArrangement = Arrangement.spacedBy(4.dp)
+        ) {
+            items(filteredItems) { item ->
+                Text(text = item)
+            }
+        }
+        Spacer(modifier = Modifier.height(200.dp))
 
         // Save button
         Button(onClick = {
